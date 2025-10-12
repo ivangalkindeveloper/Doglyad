855ec45b09bf7762f7ba852b4232c555d0c8e88f
Ivan Galkin <ivangalkindeveloper@gmail.com>
2025-10-13 01:47:24 +0300

    Small changes

--- DoglyadIOSClient/DoglyadUI/Theme/DThemeWrapperView.swift
+++ DoglyadIOSClient/DoglyadUI/Theme/DThemeWrapperView.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-public struct DThemeWrapperView<Content: View>: View  {
+public struct DThemeWrapperView<Content: View>: View {
     @StateObject private var theme = DTheme.light
     var child: Content
-    
+
     public init(
         _ child: Content
     ) {
         self.child = child
     }
-    
+
     public var body: some View {
-         child
-        .environmentObject(self.theme)
+        child
+            .environmentObject(theme)
     }
 }
